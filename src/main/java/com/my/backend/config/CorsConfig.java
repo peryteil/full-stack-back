2ef4b5591c32c730403a20d4dfe7a5c1e57af119
peryteil <peryteil@gmail.com>
2025-03-26 17:11:12 +0900

    #3

---
+++ src/main/java/com/my/backend/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.my.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedMethods("GET", "POST")
+                        .allowedOrigins("http://3.36.79.31")// react 서버 주소
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
