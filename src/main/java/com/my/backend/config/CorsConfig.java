6f079859d64a29db3f9306eb64146131aa0676fb
peryteil <peryteil@gmail.com>
2025-03-27 16:52:40 +0900

    final

--- src/main/java/com/my/backend/config/CorsConfig.java
+++ src/main/java/com/my/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedMethods("GET", "POST")
-                        .allowedOrigins("http://3.36.79.31")// react 서버 주소
+                        .allowedOrigins("http://peryteil-front-app.s3-website.ap-northeast-2.amazonaws.com")// react 서버 주소
                         .allowCredentials(true);
             }
         };
